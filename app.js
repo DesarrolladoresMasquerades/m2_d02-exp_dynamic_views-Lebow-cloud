e1f368bdca76471e64d8e61da1c59dda339391a0
Daniel Milan <danielmilanvera@gmail.com>
2022-02-08 15:57:45 +0100

    Finish Class

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const daniData = require("./dani.json")
 const express = require("express");
 
 require("dotenv").config();
@@ -18,10 +19,8 @@ app.get("/about", (req, res, next) =>
 res.render("about")
 );
 
-app.get("/", (req, res, next) => res.render("index",{
-    name: "Dani",
-    hobbies: ["Crochet", "Padel", "Gaming"]
-    }));
+app.get("/", (req, res, next) => res.render("index", daniData))
+// CLEAN DATA BEFORE RENDER
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("🏃‍ on port 3000"));
